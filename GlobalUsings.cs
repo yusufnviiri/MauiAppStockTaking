5439451bfd74eb41723d0b37f634a3a56dd6a1de
Yusuf Nviiri <yusufnviiri@gmail.com>
2024-07-08 18:11:47 +0300

    fix errors in global config

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,1 +1,8 @@
-﻿global using MauiAppStockTaking.Pages;
+﻿//global using global::System.Collections.Generic;
+//global using global::System.IO;
+//global using global::System.Linq;
+//global using global::System.Net.Http;
+//global using global::System.Threading;
+//global using global::System.Threading.Task;
+using global::MauiAppStockTaking.Pages;
+
